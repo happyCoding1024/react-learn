4a90f3134b960766a99dd8e6ce7c9cab28395325
happyCoding1024 <zhangguicheng_1995@163.com>
2019-11-18 08:37:36 +0800

    首页组件拆分

--- src/jianshu/src/pages/home/index.js
+++ src/jianshu/src/pages/home/index.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import { HomeWrapper } from './style';
+import Topic from './components/Topic';
+import List from './components/List';
+import Recommend from './components/Recommend';
+import Writer from './components/Writer';
+
+import {
+  HomeWrapper,
+  HomeLeft,
+  HomeRight
+} from './style';
+
 
 class Home extends Component {
 	render() {
 		return (
-				<HomeWrapper>Home</HomeWrapper>
+				<HomeWrapper>
+          <HomeLeft>
+            <img className='banner-img' src="//upload-images.jianshu.io/upload_images/1211570-c58cab32ffc3970f" />
+            <Topic className='banner-topic'></Topic>
+            <List></List>
+          </HomeLeft>
+          <HomeRight>
+            <Recommend></Recommend>
+            <Writer></Writer>
+          </HomeRight>
+				</HomeWrapper>
 			)
 	}
 }
--- src/jianshu/src/pages/home/style.js
+++ src/jianshu/src/pages/home/style.js
@@ -1,8 +1,23 @@
 import styled from 'styled-components';
 
 export const HomeWrapper = styled.div`
+  overflow: hidden; 
 	width: 960px;
 	margin: 0 auto;
-	height: 300px;
-	background: red;
+`;
+
+export const HomeLeft = styled.div`
+  margin-left: 15px;
+  padding-top: 30px;
+  width: 625px;
+  float: left;
+  .banner-img {
+    width: 625px;
+    height: 270px;
+  }
+`;
+
+export const HomeRight = styled.div`
+  width: 200px;
+  float: right;
 `;
