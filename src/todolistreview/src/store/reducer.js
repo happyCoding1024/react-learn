0a1b1a9301b57fd98420e74682053a4892d8da45
happyCoding1024 <zhangguicheng_1995@163.com>
2020-03-20 11:13:49 +0800

    创建store和reducer

---
+++ src/todolistreview/src/store/reducer.js
@@ -0,0 +1,12 @@
+const defaultState = {
+  inputValue: '123',
+  list: [1, 3],
+  data: [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed common'
+  ]
+}
+
+export default (state = defaultState, action) => {
+  return state;
+}
